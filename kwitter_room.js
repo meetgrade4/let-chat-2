5d0c220385077c9dcb3b7741da6ec70528d475c1
meetgrade4 <80895724+meetgrade4@users.noreply.github.com>
2021-07-17 19:24:03 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCsKC22jrwlM5Dr-BwV0AHN326WZlDvqJo",
+    authDomain: "kwitter-redesigned.firebaseapp.com",
+    databaseURL: "https://kwitter-redesigned-default-rtdb.firebaseio.com",
+    projectId: "kwitter-redesigned",
+    storageBucket: "kwitter-redesigned.appspot.com",
+    messagingSenderId: "36970943548",
+    appId: "1:36970943548:web:fad94ebe16d2094a6ddf39"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  
